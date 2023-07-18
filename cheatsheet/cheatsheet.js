f06cbfaea811d1c318872344dfb459edd214fda4
João Matheus Silva Franca <matheussfranca@live.com>
2023-07-18 14:42:50 -0300

    Adicionando dados do tipo Array e Objectos

--- cheatsheet/cheatsheet.js
+++ cheatsheet/cheatsheet.js
@@ -3,10 +3,13 @@ var variable01 = null;
 let variable02 = null;
 const variable03 = null;
 
-// Tipos primitivos de dados:
+// Tipos de dados:
 const stringVariable = 'Olá eu sou uma string';
 const numberVariable = 33;
 const booleanVariable00 = false;
 const booleanVariable01 = true;
 const undefinedVariable = undefined;
 const nullVariable = null;
+const arrayVariable = [0, 1, 2, 3, 4, 5];
+const objectVariable = { chave: 'valor'};
+
