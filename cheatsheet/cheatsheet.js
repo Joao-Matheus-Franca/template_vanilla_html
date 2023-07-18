eed9302df532ed7dcd34653126d481d2a77bb3e8
João Matheus Silva Franca <matheussfranca@live.com>
2023-07-18 14:36:51 -0300

    Adicionando formas de declarar variáveis e tipos primitivos

---
+++ cheatsheet/cheatsheet.js
@@ -0,0 +1,12 @@
+// Formas de declarar variáveis:.
+var variable01 = null;
+let variable02 = null;
+const variable03 = null;
+
+// Tipos primitivos de dados:
+const stringVariable = 'Olá eu sou uma string';
+const numberVariable = 33;
+const booleanVariable00 = false;
+const booleanVariable01 = true;
+const undefinedVariable = undefined;
+const nullVariable = null;
